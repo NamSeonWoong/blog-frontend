0e2c0919411b94678cea9d7746b6d2ea7af56da8
남선웅 <wun1107@gmail.com>
2020-10-15 01:29:13 +0900

    로그아웃 구현

--- src/modules/user.js
+++ src/modules/user.js
@@ -10,11 +10,11 @@ const TEMP_SET_USER = 'user/TEMP_SET_USER'; // 새로고침 이후 임시 로그
 const [CHECK, CHECK_SUCCESS, CHECK_FAILURE] = createRequestActionTypes(
   'user/CHECK',
 );
-// const LOGOUT = 'user/LOGOUT';
+const LOGOUT = 'user/LOGOUT';
 
 export const tempSetUser = createAction(TEMP_SET_USER, user => user);
 export const check = createAction(CHECK);
-// export const logout = createAction(LOGOUT);
+export const logout = createAction(LOGOUT);
 
 const checkSaga = createRequestSaga(CHECK, authAPI.check);
 
@@ -26,19 +26,19 @@ function checkFailureSaga() {
   }
 }
 
-// function* logoutSaga() {
-//   try {
-//     yield call(authAPI.logout); // logout API 호출
-//     localStorage.removeItem('user'); // localStorage 에서 user 제거
-//   } catch (e) {
-//     console.log(e);
-//   }
-// }
+function* logoutSaga() {
+  try {
+    yield call(authAPI.logout); // logout API 호출
+    localStorage.removeItem('user'); // localStorage 에서 user 제거
+  } catch (e) {
+    console.log(e);
+  }
+}
 
 export function* userSaga() {
   yield takeLatest(CHECK, checkSaga);
   yield takeLatest(CHECK_FAILURE, checkFailureSaga);
-  // yield takeLatest(LOGOUT, logoutSaga);
+  yield takeLatest(LOGOUT, logoutSaga);
 }
 
 const initialState = {
@@ -62,10 +62,10 @@ export default handleActions(
       user: null,
       checkError: error,
     }),
-    // [LOGOUT]: state => ({
-    //   ...state,
-    //   user: null,
-    // }),
+    [LOGOUT]: state => ({
+      ...state,
+      user: null,
+    }),
   },
   initialState,
 );
