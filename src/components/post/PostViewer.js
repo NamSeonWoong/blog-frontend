1bdec2759859953a6f969c0293fbcd0eb4d22a66
남선웅 <wun1107@gmail.com>
2020-10-19 21:27:12 +0900

    포스트 수정

--- src/components/post/PostViewer.js
+++ src/components/post/PostViewer.js
@@ -2,9 +2,9 @@ import React from 'react';
 import styled from 'styled-components';
 import palette from '../../lib/styles/palette';
 import Responsive from '../common/Responsive';
-
 import SubInfo from '../common/SubInfo';
 import Tags from '../common/Tags';
+// import { Helmet } from 'react-helmet-async';
 
 const PostViewerBlock = styled(Responsive)`
   margin-top: 4rem;
@@ -25,7 +25,7 @@ const PostContent = styled.div`
   color: ${palette.gray[8]};
 `;
 
-const PostViewer = ({ post, error, loading }) => {
+const PostViewer = ({ post, error, loading,actionButtons }) => {
   // 에러 발생 시
   if (error) {
     if (error.response && error.response.status === 404) {
@@ -51,6 +51,7 @@ const PostViewer = ({ post, error, loading }) => {
         />
         <Tags tags={tags} />
       </PostHead>
+      {actionButtons}
       <PostContent dangerouslySetInnerHTML={{ __html: body }} />
     </PostViewerBlock>
   );
